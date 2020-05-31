9170c5f5de12d1ffef2400d11029c2d35052a4f5
prabhat00155 <prabhatr@usc.edu>
2020-05-31 21:52:11 +0530

    Adding flutter project which displays a screen titled Vote!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vote
+# Vote
+
 Vote is an online service where people can create polls and vote on them anonymously.
---
+++ android/app/src/main/kotlin/com/example/vote/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
