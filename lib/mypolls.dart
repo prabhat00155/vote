6ad01a929157fcceddd7dd97c9e134e6e34312a5
Prabhat <prabhatr@usc.edu>
2020-06-11 23:14:09 +0530

    Prabhat00155/add csv (#9)
    * Added linear percentage indicator
    
    * Re-order imports to make them alphabetical
    
    * Removed hard-coded sqlite db and modularised code
    
    * Address review comments

--- lib/mypolls.dart
+++ lib/mypolls.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 
 import 'questions.dart';
 
@@ -8,110 +7,17 @@ class MyPolls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var mypolls = questions();
     return Scaffold(
       appBar: AppBar(
         title: Text("My Polls"),
       ),
-      body: FutureBuilder<List>(
-          future: questions(),
-          initialData: List(),
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              snapshot.data[index].question,
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(3, 3, 10, 3),
-                              width: double.infinity,
-                              child: LinearPercentIndicator(
-                                animation: true,
-                                animationDuration: 500,
-                                width: 140.0,
-                                lineHeight: 20.0,
-                                percent: 0.5,
-                                center: Text(
-                                  snapshot.data[index].option1,
-                                  style: TextStyle(fontSize: 18.0),
-                                ),
-                                linearStrokeCap: LinearStrokeCap.roundAll,
-                                backgroundColor: Colors.grey[100],
-                                progressColor: Colors.blue[50],
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(3, 3, 10, 3),
-                              width: double.infinity,
-                              child: LinearPercentIndicator(
-                                animation: true,
-                                animationDuration: 500,
-                                width: 140.0,
-                                lineHeight: 20.0,
-                                percent: 0.3,
-                                center: Text(
-                                  snapshot.data[index].option2,
-                                  style: TextStyle(fontSize: 18.0),
-                                ),
-                                linearStrokeCap: LinearStrokeCap.roundAll,
-                                backgroundColor: Colors.grey[100],
-                                progressColor: Colors.blue[50],
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(3, 3, 10, 3),
-                              width: double.infinity,
-                              child: snapshot.data[index].option3 == null
-                                  ? Offstage()
-                                  : LinearPercentIndicator(
-                                      animation: true,
-                                      animationDuration: 500,
-                                      width: 140.0,
-                                      lineHeight: 20.0,
-                                      percent: 0.1,
-                                      center: Text(
-                                        snapshot.data[index].option3,
-                                        style: TextStyle(fontSize: 18.0),
-                                      ),
-                                      linearStrokeCap: LinearStrokeCap.roundAll,
-                                      backgroundColor: Colors.grey[100],
-                                      progressColor: Colors.blue[50],
-                                    ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(3, 3, 10, 3),
-                              width: double.infinity,
-                              child: snapshot.data[index].option4 == null
-                                  ? Offstage()
-                                  : LinearPercentIndicator(
-                                      animation: true,
-                                      animationDuration: 500,
-                                      width: 140.0,
-                                      lineHeight: 20.0,
-                                      percent: 0.1,
-                                      center: Text(
-                                        snapshot.data[index].option4,
-                                        style: TextStyle(fontSize: 18.0),
-                                      ),
-                                      linearStrokeCap: LinearStrokeCap.roundAll,
-                                      backgroundColor: Colors.grey[100],
-                                      progressColor: Colors.blue[50],
-                                    ),
-                            ),
-                          ],
-                        ),
-                      );
-                    })
-                : Center(
-                    child: CircularProgressIndicator(),
-                  );
-          }),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(child: displayPoll(mypolls[index]));
+        },
+        itemCount: mypolls.length,
+      ),
     );
   }
 }
--- lib/questions.dart
+++ lib/questions.dart
@@ -1,55 +1,97 @@
-import 'dart:async';
-
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Question {
-  final int questionId;
+  final String questionId;
   final String userId;
   final String question;
-  final String option1;
-  final String option2;
-  final String option3;
-  final String option4;
-  Question(
-      {this.questionId,
-      this.userId,
-      this.question,
-      this.option1,
-      this.option2,
-      this.option3,
-      this.option4});
-
-  Map<String, dynamic> toMap() {
-    return {
-      'question': question,
-      'option1': option1,
-      'option2': option2,
-    };
-  }
+  final List<String> options;
+  final List<String> votes;
+  final String totalVotes;
+  Question({
+    this.questionId,
+    this.userId,
+    this.question,
+    this.options,
+    this.votes,
+    this.totalVotes,
+  });
 }
 
-Future<List<Question>> questions() async {
-  // Hard-coded DB location for the time-being.
-  final Future<Database> database = openDatabase(
-    join('/Users/prroy/Documents/Apps/flutter_projects/databases/', 'vote.db'),
+List<Question> questions() {
+  return dataToQuestion(data);
+}
+
+double calculateFraction(vote, total) {
+  if (vote == '') return 0;
+  return double.parse(vote) / double.parse(total);
+}
+
+Column displayPoll(data) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        data.question,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      for (int i = 0; i < data.options.length; i++)
+        displayOption(
+            data.options[i], calculateFraction(data.votes[i], data.totalVotes)),
+    ],
   );
-  // Get a reference to the database.
-  final Database db = await database;
+}
 
-  // Query the table for all the questions.
-  final List<Map<String, dynamic>> maps = await db.query('questions');
+Container displayOption(option, percent) {
+  return Container(
+    margin: EdgeInsets.fromLTRB(3, 3, 10, 3),
+    width: double.infinity,
+    child: option == null || option == ''
+        ? Offstage()
+        : LinearPercentIndicator(
+            animation: true,
+            animationDuration: 500,
+            width: 140.0,
+            lineHeight: 20.0,
+            percent: percent,
+            center: Text(
+              option,
+              style: TextStyle(fontSize: 18.0),
+            ),
+            linearStrokeCap: LinearStrokeCap.roundAll,
+            backgroundColor: Colors.grey[100],
+            progressColor: Colors.blue[50],
+          ),
+  );
+}
 
-  // Convert the List<Map<String, dynamic> into a List<Question>.
-  return List.generate(maps.length, (i) {
-    return Question(
-      questionId: maps[i]['question_id'],
-      userId: maps[i]['user_id'],
-      question: maps[i]['question'],
-      option1: maps[i]['option1'],
-      option2: maps[i]['option2'],
-      option3: maps[i]['option3'],
-      option4: maps[i]['option4'],
-    );
-  });
+const List<String> data = [
+  "1,anon1,Is this app good?,Yes,No,,,,62,61,,,,123",
+  "2,anon1,Are you happy with your life?,Yes,No,,,,76,69,,,,145",
+  "3,anon1,Which is better?,Facebook,WhatsApp,TikTok,Instagram,Twitter,28,28,24,24,20,124",
+  "4,anon1,Favourite holiday destination?,Maldives,Mauritius,Hawaii,New Zealand,,22,22,19,35,,98",
+  "5,anon1,Where would you prefer living?,Island,Mountains,,,,36,75,,,,111",
+  "6,anon1,What do you use?,iPhone,Android phone,,,,407,827,,,,1234",
+  "7,anon1,What's more important for you?,Money,Happiness,Peace,,,85,110,50,,,245",
+  "8,anon1,What's worse for body?,Sugar,Salt,,,,187,380,,,,567",
+  "9,anon1,Your favourite sports?,Cricket,Football,Tennis,Formula 1,Badminton,99,99,86,86,62,432",
+  "10,anon1,Whom would you say you idolise?,Mahatma Gandhi,John Lennon,Mother Teresa,Nelson Mandela,Martin Luther King Jr.,3,2,1,1,2,9",
+];
+
+List<Question> dataToQuestion(data) {
+  List<Question> questions = [];
+  data.forEach((row) => questions.add(parseRow(row)));
+  return questions;
+}
+
+Question parseRow(row) {
+  var items = row.split(",");
+  return Question(
+    questionId: items[0],
+    userId: items[1],
+    question: items[2],
+    options: items.sublist(3, 8),
+    votes: items.sublist(8, 13),
+    totalVotes: items[13],
+  );
 }
