06e092192edf2c65d9580dbe6d1994fe529ec178
Prabhat <prabhatr@usc.edu>
2020-06-25 18:39:51 +0530

    Added timeline with SliverAppBar (#18)

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
+import 'questions.dart';
+
 class Timeline extends StatelessWidget {
   const Timeline({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
+      body: FutureBuilder<List>(
+        future: fetchQuestions(),
+        initialData: List(),
+        builder: (context, snapshot) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                floating: true,
+                flexibleSpace: const FlexibleSpaceBar(
+                  title: Text('Vote!'),
+                ),
+              ),
+              snapshot.hasData
+                  ? SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) => Card(
+                          child: displayPoll(snapshot.data[index]),
+                        ),
+                        childCount: snapshot.data.length,
+                      ),
+                    )
+                  : SliverToBoxAdapter(
+                      child: CircularProgressIndicator(),
+                    ),
+            ],
+          );
+        },
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Lorem Ipsum'),
-          subtitle: Text('$index'),
-        );
-      }),
     );
   }
 }
